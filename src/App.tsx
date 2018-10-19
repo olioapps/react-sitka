aee5d1091c868e1e445ba833bfac66751eb8565a
Jon <jon.walz.music@gmail.com>
2018-10-18 17:22:51 -0700

    tudehs werk

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,60 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react"
+import "./App.css"
 import { connect } from "react-redux"
-import {  SitkaInstance } from "./index"
+import { SitkaInstance } from "./index"
 
 import { AppState, TestState, sitka, Sitka, SitkaModules } from "./sitka"
 
-class ChildComponent extends React.Component<TestState, {}> {
-    constructor(props: TestState) {
-        super(props)
-    }
+class ChildComponent extends React.Component<AppState, {}> {
     public render() {
+        /* tslint:disable */
+        debugger
+        /* tslint:enable */
         return (
             <div>
                 <div>{JSON.stringify(this.props, null, 4)}</div>
-                <button onClick={() => {
-                    SitkaInstance.getModules().test.handleIncrementCount()}
-                }>Click</button>
+                <button
+                    onClick={() => {
+                        SitkaInstance.getModules().test.handleIncrementCount()
+                    }}>
+                    Click
+                </button>
             </div>
         )
     }
 }
 
-interface AppProps {}
 
-class App extends React.Component<AppProps> {
-  public render() {
-    return (
-        <ChildComponent />
-    );
-  }
+// export type ReduxProps = {
+//     readonly test: TestState
+//     readonly test2: Test2State
+//     readonly test3: Test3State
+//     // readonly dispatch: {}
+// }
+
+interface AppProps {
+    // readonly sitka: Sitka
+    // readonly test: TestState
+}
+
+class App extends React.Component<{}, {}> {
+    public render() {
+        return <ChildComponent {...this.props} />
+    }
 }
 
 // export default App
-const mapStateToProps = (
-    state: AppState,
-): AppState => {
+const mapStateToProps = (state: AppState): AppState => {
     return state
 }
 
+// export default connect(
+//     mapStateToProps,
+//     null,
+// )(App)
+const ConnectedApp: React.ComponentClass<AppProps> = connect(
+    mapStateToProps,
+    null,
+)(App)
 
-export default connect(mapStateToProps, null)(App);
+export default ConnectedApp
